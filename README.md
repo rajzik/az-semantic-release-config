f70a29160ce758c11dac08552454b4baa93a1d23
Jan Šilhan <silhanu@gmail.com>
2020-02-24 18:06:41 +0100

    breaking: Initial commit.

---
+++ README.md
@@ -0,0 +1,29 @@
+# @rajzik/az-semantic-release-config
+
+[**semantic-release**](https://github.com/semantic-release/semantic-release) shareable config to publish npm packages with [GitHub](https://github.com).
+
+## Plugins
+
+This [shareable configuration](https://github.com/jedmao/semantic-release-npm-github-config/blob/master/.releaserc.json) uses the following plugins:
+
+- [`@semantic-release/commit-analyzer`](https://github.com/semantic-release/commit-analyzer)
+- [`@semantic-release/release-notes-generator`](https://github.com/semantic-release/release-notes-generator)
+- [`@semantic-release/npm`](https://github.com/semantic-release/npm)
+- [`@semantic-release/git`](https://github.com/semantic-release/git)
+
+## Install
+
+```bash
+npm install --save-dev semantic-release @rajzik/az-semantic-release-config
+```
+
+## Usage
+
+The shareable config can be configured in the [**semantic-release** configuration file](https://github.com/semantic-release/semantic-release/blob/master/docs/usage/configuration.md#configuration):
+
+```json
+{
+  "extends": "@rajzik/az-semantic-release-config",
+  "branch": "master"
+}
+```
--- index.js
+++ index.js
@@ -1,41 +1,42 @@
 module.exports = {
   plugins: [
     [
-      "@semantic-release/commit-analyzer",
+      '@semantic-release/commit-analyzer',
       {
-        config: "conventional-changelog-beemo",
+        config: 'conventional-changelog-beemo',
         releaseRules: [
-          { type: "break", release: "major" },
-          { type: "breaking", release: "major" },
-          { type: "release", release: "major" },
-          { type: "new", release: "minor" },
-          { type: "update", release: "minor" },
-          { type: "feature", release: "minor" },
-          { type: "fix", release: "patch" },
-          { type: "deps", release: "patch" },
-          { type: "docs", release: "patch" },
-          { type: "revert", release: "patch" },
-          { type: "style", release: "patch" },
-          { type: "security", release: "patch" },
-          { type: "type", release: "patch" },
-          { type: "types", release: "patch" },
-          { type: "misc", release: "patch" }
-        ]
-      }
+          { type: 'break', release: 'major' },
+          { type: 'breaking', release: 'major' },
+          { type: 'release', release: 'major' },
+          { type: 'new', release: 'minor' },
+          { type: 'update', release: 'minor' },
+          { type: 'feature', release: 'minor' },
+          { type: 'fix', release: 'patch' },
+          { type: 'deps', release: 'patch' },
+          { type: 'docs', release: 'patch' },
+          { type: 'revert', release: 'patch' },
+          { type: 'style', release: 'patch' },
+          { type: 'security', release: 'patch' },
+          { type: 'type', release: 'patch' },
+          { type: 'types', release: 'patch' },
+          { type: 'misc', release: 'patch' },
+        ],
+      },
     ],
     [
-      "@semantic-release/release-notes-generator",
+      '@semantic-release/release-notes-generator',
       {
-        config: "conventional-changelog-beemo"
-      }
+        config: 'conventional-changelog-beemo',
+      },
     ],
-    "@semantic-release/changelog",
-    "@semantic-release/npm",
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
     [
-      "@semantic-release/git",
+      '@semantic-release/git',
       {
-        message: "ci(release): V${nextRelease.version} [ci skip]."
-      }
-    ]
-  ]
-};
+        message:
+          'ci(release): V${nextRelease.version} [ci skip].\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+}
